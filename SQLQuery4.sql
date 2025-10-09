9384f9a678385fda4f93e859fdbafcf04ba9f299
rawdaabdelsalam42 <rawdaabdelsalam42@gmail.com>
2025-10-09 15:45:57 +0300

    Update SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,24 +1,24 @@
-﻿-- 1) إجمالي المبيعات لكل فئة (Category)
+﻿-- إجمالي المبيعات لكل فئة (Category)
 SELECT Category, SUM(Total) AS TotalSales
 FROM dbo.finaldata
 GROUP BY Category
 ORDER BY TotalSales DESC;
 
 
--- 2) عدد العملاء حسب النوع (Gender)
+-- عدد العملاء حسب النوع (Gender)
 SELECT Gender, COUNT(DISTINCT CustomerID) AS NumCustomers
 FROM dbo.finaldata
 GROUP BY Gender;
 
 
--- 3) أعلى 5 عملاء صرفوا فلوس
+--  أعلى 5 عملاء صرفوا فلوس
 SELECT TOP (5) CustomerID, SUM(Total) AS TotalSpent
 FROM dbo.finaldata
 GROUP BY CustomerID
 ORDER BY TotalSpent DESC;
 
 
--- 4) إجمالي المبيعات بالشهور (مع تحويل OrderDate لنوع DATE)
+-- إجمالي المبيعات بالشهور (مع تحويل OrderDate لنوع DATE)
 SELECT FORMAT(TRY_CAST(OrderDate AS DATE), 'yyyy-MM') AS [Month],
        SUM(Total) AS TotalSales
 FROM dbo.finaldata
@@ -27,7 +27,7 @@ GROUP BY FORMAT(TRY_CAST(OrderDate AS DATE), 'yyyy-MM')
 ORDER BY [Month];
 
 
--- 5) أكثر 5 منتجات مبيعًا
+--  أكثر 5 منتجات مبيعًا
 SELECT TOP (5) ProductID, SUM(Quantity) AS TotalQuantity
 FROM dbo.finaldata
 GROUP BY ProductID
